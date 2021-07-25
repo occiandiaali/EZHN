a2badaa801e09ccd34051d3f9eeca3a9456ba05d
occiandiaali <ocean.diaali@gmail.com>
2021-07-25 18:38:59 +0100

    first commit

--- src/screens/AboutMeScreen.js
+++ src/screens/AboutMeScreen.js
@@ -3,10 +3,28 @@ import {StyleSheet, Text, View} from 'react-native';
 
 export default function AboutMeScreen() {
   return (
-    <View>
-      <Text>About Me</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Occian F. Diaali</Text>
+      <Text style={styles.sub}>Mobile &amp; Web Developer</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 12,
+  },
+  heading: {
+    fontSize: 55,
+    color: 'gray',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+  sub: {
+    color: '#ff0000',
+    fontSize: 27,
+  },
+});
