28c1511297b36a3bfbd4b5bd8134fa69f973a7f0
occiandiaali <ocean.diaali@gmail.com>
2021-07-26 09:02:14 +0100

    add Paper menu for navigation

--- src/components/CustomNava.js
+++ src/components/CustomNava.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+//import {TouchableOpacity} from 'react-native';
 
 import {useLinkTo} from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function CustomNava({name, size, onPress}) {
+import {Button, Divider, Menu} from 'react-native-paper';
+
+export default function CustomNava() {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
   const linkTo = useLinkTo();
   return (
-    <TouchableOpacity onPress={() => linkTo('/Login')}>
-      <Icon style={{paddingRight: 25}} name={name} color="black" size={size} />
-    </TouchableOpacity>
+    <Menu
+      visible={visible}
+      onDismiss={closeMenu}
+      anchor={
+        <Button mode="text" color="black" onPress={openMenu}>
+          <Icon name="menu" color="#000" size={30} />
+        </Button>
+      }>
+      <Menu.Item
+        onPress={() => {
+          linkTo('/About');
+          closeMenu();
+        }}
+        title="About Me"
+      />
+      <Menu.Item
+        onPress={() => {
+          linkTo('/Settings');
+          closeMenu();
+        }}
+        title="Settings"
+      />
+      <Divider />
+      <Menu.Item
+        onPress={() => {
+          linkTo('/Login');
+          closeMenu();
+        }}
+        title="Login"
+      />
+    </Menu>
+    // <TouchableOpacity onPress={() => linkTo('/Login')}>
+    //   <Icon style={{paddingRight: 25}} name={name} color="black" size={size} />
+    // </TouchableOpacity>
   );
 }
