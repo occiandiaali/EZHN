aa953d857a4e6d95496e39569a6a3d306ab57020
occiandiaali <ocean.diaali@gmail.com>
2021-07-26 10:02:11 +0100

    add signup fields and style

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -12,74 +12,121 @@ export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={require('../res/images/hacker.png')}
-        accessibilityLabel="Easy Hacker News"
-      />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="you@emailaddress.com"
-          placeholderTextColor="#003f5c"
-          onChangeText={email => setEmail(email)}
+    <>
+      <View style={styles.container_log}>
+        <Image
+          style={styles.image}
+          source={require('../res/images/ehn_logo.png')}
+          accessibilityLabel="Easy Hacker News"
         />
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="you@emailaddress.com"
+            placeholderTextColor="#003f5c"
+            onChangeText={email => setEmail(email)}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="your password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={password => setPassword(password)}
+          />
+        </View>
+        {/* <TouchableOpacity>
+          <Text style={styles.reg_button}>No Account? Register</Text>
+        </TouchableOpacity> */}
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="your password"
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={password => setPassword(password)}
-        />
+      <Text style={styles.txt}>OR</Text>
+
+      <View style={styles.container_reg}>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="enter email"
+            placeholderTextColor="#003f5c"
+            onChangeText={email => setEmail(email)}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="enter password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={password => setPassword(password)}
+          />
+        </View>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.regText}>SIGN UP</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  container_log: {
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 20,
   },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
+  container_reg: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 25,
+    marginBottom: 20,
   },
   image: {
-    width: 150,
-    height: 150,
-    marginBottom: 40,
+    width: 100,
+    height: 100,
+    marginBottom: 35,
   },
   inputView: {
     backgroundColor: '#FFC0CB',
     borderRadius: 30,
     width: '70%',
     height: 45,
-    marginBottom: 20,
+    marginBottom: 10,
     alignItems: 'center',
   },
   loginBtn: {
     width: '80%',
     borderRadius: 25,
-    height: 50,
+    height: 45,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 10,
+    marginTop: 3,
     backgroundColor: '#FF1493',
   },
+  loginText: {
+    fontWeight: 'bold',
+    fontSize: 26,
+  },
+  reg_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  regText: {
+    fontWeight: 'bold',
+    fontSize: 26,
+  },
+  txt: {
+    alignSelf: 'center',
+    fontSize: 21,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: 'gray',
+  },
   TextInput: {
-    height: 50,
+    height: 40,
     flex: 1,
     padding: 10,
     marginLeft: 20,
