7a8a00c7d8dbc801f8b710b2e3892bbcbc876b0b
occiandiaali <ocean.diaali@gmail.com>
2021-07-26 11:39:49 +0100

    first commit

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default function LoginComponent({text, placeholder, onChangeText}) {
+  return (
+    <View style={styles.inputView}>
+      <TextInput
+        style={styles.inputText}
+        value={text}
+        placeholder={placeholder}
+        placeholderTextColor="#003f5c"
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputText: {
+    height: 40,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  inputView: {
+    backgroundColor: '#FFC0CB',
+    borderRadius: 30,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
